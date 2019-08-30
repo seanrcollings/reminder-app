29c267bd72367981a089132b9057b8fad762fea6
Sean Collings <seanrcollings@gmail.com>
2019-08-30 16:03:07 -0600

    still trying to figure this out

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+import time
 
 class Reminder(BoxLayout):
-  def __init__(self, **kwargs):
+  def __init__(self, name, deadline, **kwargs):
     super(Reminder, self).__init__(**kwargs)
-    self.name = 'test'
-    self.time = 'test'    
+    name = Property('test')
+    # self.deadline = deadline + time.time()    
   
 
 class ReminderGrid(BoxLayout):
   def __init__(self, **kwargs):
     super(ReminderGrid, self).__init__(**kwargs)
-    self.reminders = [{'text': 'test', 'value': 1}, {'text': 'test2', 'value': 1}]
-    self.recycle.data = self.reminders
+    self.reminders = [Reminder(name='Reminder 1', deadline=12), Reminder(name='Reminder 2', deadline=13)]
 
+    
 class ReminderApp(App):
   def build(self):
-    return ReminderGrid()
+    return Reminder(name='Test', deadline=12)
 
 ReminderApp().run()
